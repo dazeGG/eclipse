e4ef9c126216f4625ccceefd0fe13a25b0ba513f
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-24 14:50:14 +0300

    title changes

--- src/pages/ConverterPage.vue
+++ src/pages/ConverterPage.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Converter</h1>
+  <div class="title">
+    <h1>Конвертер</h1>
+    <div class="postscript">
+      <b>[</b>Работает через RUB<b>]</b>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.title {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  margin: 20px 0;
+}
+
+.postscript {
+  font-size: 26px;
+  font-weight: 500;
+}
+</style>
