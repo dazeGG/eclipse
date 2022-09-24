7be7bbba7f2833258938f75943b8f208e9c072be
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-24 10:26:39 +0300

    store setup

--- src/pages/CurrenciesPage.vue
+++ src/pages/CurrenciesPage.vue
@@ -1,3 +1,16 @@
 <template>
   <h1>Список валют</h1>
+  {{ currencies }}
 </template>
+
+<script setup lang="ts">
+import { onBeforeMount, computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const currencies = computed(() => store.getters.getCurrencies)
+
+onBeforeMount(async () => {
+  await store.dispatch('getCurrencies')
+})
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
+const api = axios.create({ baseURL: 'https://www.cbr-xml-daily.ru' })
+
+interface ICurrency {
+  ID: string,
+  NumCode: string,
+  CharCode: string,
+  Nominal: number,
+  Name: string,
+  Value: number,
+  Previous: number
+}
 
 export default createStore({
   state: {
+    currencies: []
   },
   getters: {
+    getCurrencies: state => state.currencies
   },
   mutations: {
+    setCurrencies (state: any, payload: ICurrency[]) {
+      state.currencies = payload
+    }
   },
   actions: {
-  },
-  modules: {
+    async getCurrencies ({ commit }: any) {
+      const res = await api.get('/daily_json.js')
+      commit('setCurrencies', Object.keys(res.data.Valute).map((currency: string): ICurrency => res.data.Valute[currency]))
+    }
   }
 })
