66ceab9a2c39c62d4be94c24bae3a1954d42be71
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-24 15:37:01 +0300

    added AppSelect.vue

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <select v-model="value" class="input converter__currency" @change="emit('select', value)">
+    <option v-for="currency in currencies" :key="currency.ID">
+      {{ currency.CharCode }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, defineProps, defineEmits } from 'vue'
+import { useStore } from 'vuex'
+
+const props = defineProps({
+  selectedValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['select'])
+
+const store = useStore()
+
+const currencies = computed(() => store.getters.getCurrencies)
+
+const value = ref(props.selectedValue)
+</script>
